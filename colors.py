c1839b731289b0c386a1a73736f5f17fa6c9687d
Carlos Flores <darthmincho@gmail.com>
2015-04-20 18:26:25 -0400

    Add pastel colors list to colors.py

---
+++ colors.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+
+PASTEL_COLORS = (
+	'#F7977A', 
+	'#F9AD81',
+	'#FDC68A',
+	'#FFF79A',
+	'#C4DF9B',
+	'#A2D39C',
+	'#82CA9D',
+	'#7BCDC8',
+	'#6ECFF6',
+	'#7EA7D8',
+	'#8493CA',
+	'#8882BE',
+	'#A187BE',
+	'#BC8DBF',
+	'#F49AC2',
+	'#F6989D'
+)
